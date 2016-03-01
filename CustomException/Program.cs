063c8b92a59f8cafe477ff9b02623c1ba51d934e
hsabillon7 <h.hsabillon@gmail.com>
2016-03-01 14:54:43 -0600

    Utilizando una ArgumentOutOfRangeException y la cláusula finally

---
+++ CustomException/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Excepciones Personalizadas *****\n");
+            Vehiculo miVehiculo = new Vehiculo("Carcachita", 120);
+
+            try
+            {
+                // Lanzar excepción
+                miVehiculo.Acelerar(-15);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                // Esto ocurrirá independientemente de si sucede
+                // una excepción o no...
+                miVehiculo.UtilizarRadio(false);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CustomException/Vehiculo.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomException
+{
+    class Vehiculo
+    {
+        // Constante para velocidad máxima
+        public const int VelocidadMaxima = 200;
+
+        // Propiedades del vehículo
+        public int VelocidadActual { get; set; }
+        public string Apodo { get; set; }
+
+        // El vehículo es funcional?
+        private bool vehiculoMuerto;
+
+        // El vehículo tiene una radio
+        private Radio elRadio = new Radio();
+
+        // Constructores
+        public Vehiculo() { }
+        public Vehiculo(string apodo, int velocidad)
+        {
+            VelocidadActual = velocidad;
+            Apodo = apodo;
+        }
+
+        public void UtilizarRadio(bool estado)
+        {
+            // Delegar el objeto interno
+            elRadio.Encender(estado);
+        }
+
+        // Ver si el vehículo se encuentra sobrecalentado
+        // Esta vez, lanzaremos una excepción si el usuario acelera
+        // después de la velocidad máxima
+        public void Acelerar(int delta)
+        {
+            if (delta <= 0)
+                throw new ArgumentOutOfRangeException("delta", "!La velocidad debe ser mayor que cero!");
+
+            if (vehiculoMuerto)
+                Console.WriteLine("{0} está fuera de servicio...", Apodo);
+            else
+            {
+                VelocidadActual += delta;
+
+                if (VelocidadActual > VelocidadMaxima)
+                {
+                    VelocidadActual = 0;
+                    vehiculoMuerto = true;
+                }
+            }
+        }
+    }
+}
